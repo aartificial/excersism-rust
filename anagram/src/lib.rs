9ef6a0ccec4c9851ab176151f01d0d7c555ca11b
sol <sol.recieve@gmail.com>
2023-09-30 12:58:01 +0200

    exercise: anagram

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,5 +1,24 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    todo!("For the '{word}' word find anagrams among the following words: {possible_anagrams:?}");
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let lower_word = word.to_lowercase();
+    let sorted_word = sort_word(&lower_word);
+
+    possible_anagrams
+        .iter()
+        .filter(|candidate| {
+            let lower_candidate = candidate.to_lowercase();
+            lower_candidate.len() == lower_word.len()
+                && lower_candidate != lower_word
+                && sort_word(&lower_candidate) == sorted_word
+
+        })
+        .copied()
+        .collect()
+}
+
+fn sort_word(word: &str) -> Vec<char> {
+    let mut sorted_word: Vec<char> = word.chars().collect();
+    sorted_word.sort_unstable();
+    sorted_word
 }
