0fcb3d1ce76141b6a0279a1eeb3f9e5efc4dc3cc
sol <sol.recieve@gmail.com>
2023-10-07 23:33:48 +0200

    improve: grains

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,10 +1,11 @@
 pub fn square(s: u32) -> u64 {
-    match s {
-        n if n <= 0 || n > 64 => panic!("Square must be between 1 and 64"),
-        n => 2u64.pow(n - 1),
+    if s == 0 || s > 64 {
+        panic!("Square must be between 1 and 64");
     }
+
+    1u64 << (s - 1)
 }
 
 pub fn total() -> u64 {
-    (1..64).fold(1u64, |acc, step| acc + 2u64.pow(step))
+    u64::MAX
 }
